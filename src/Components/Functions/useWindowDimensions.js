d12c5b586e462eaa7a283b38b10039fedc4a9f20
Aldo <aldoraduenzjunior@gmail.com>
2025-06-08 02:08:31 -0300

    remove log

---
+++ src/Components/Functions/useWindowDimensions.js
@@ -0,0 +1,41 @@
+import { useState, useEffect } from "react";
+
+function getWindowDimensions() {
+  if (typeof window === "undefined") {
+    return { innerWidth: 0 };
+  }
+
+  const { innerWidth } = window;
+  const classBody = document.body.classList;
+
+  if (innerWidth > 1024 && !classBody.contains("desktop")) {
+    document.body.className = "desktop";
+  } else if (
+    innerWidth > 520 &&
+    innerWidth <= 1024 &&
+    !classBody.contains("tablet")
+  ) {
+    document.body.className = "tablet";
+  } else if (innerWidth < 520 && !classBody.contains("mobile")) {
+    document.body.className = "mobile";
+  }
+
+  return innerWidth;
+}
+
+export default function useWindowDimensions() {
+  const [windowDimensions, setWindowDimensions] = useState(
+    getWindowDimensions()
+  );
+
+  useEffect(() => {
+    function handleResize() {
+      setWindowDimensions(getWindowDimensions());
+    }
+
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  return windowDimensions;
+}
